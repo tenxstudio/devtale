2241002762531842754628cbdafcb5cdce15f830
Alberto Gaona <albertoo_3c@hotmail.com>
2023-08-07 13:25:54 -0600

    process full respository

--- cli.py
+++ cli.py
@@ -21,17 +21,50 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
-def document_folder(
-    folder_path: str,
+def process_repository(
+    root_path: str,
     output_path: str = DEFAULT_OUTPUT_PATH,
     model_name: str = DEFAULT_MODEL_NAME,
 ) -> None:
-    if not os.path.exists(output_path):
-        os.makedirs(output_path)
+    folders = {}
+    folder_tales = []
+    for folder_path, _, filenames in os.walk(root_path):
+        for filename in filenames:
+            file_relative_path = os.path.relpath(
+                os.path.join(folder_path, filename), root_path
+            )
+            folder_name, file_name = os.path.split(file_relative_path)
+            # useful to keep a tree, we should use .gitignore to filter
+            if folder_name not in folders:
+                folders[folder_name] = [file_name]
+            else:
+                folders[folder_name].append(file_name)
 
+    for folder_name in folders.keys():
+        folder_path = os.path.join(root_path, folder_name)
+        folder_tale = document_folder(folder_path, output_path)
+        if folder_tale is not None:
+            folder_tales.append(
+                {"folder_name": folder_name, "folder_summary": folder_tale}
+            )
+
+    if folder_tales:
+        root_index = get_tale_index(folder_tales)
+
+        save_path = os.path.join(output_path, root_path)
+        logger.info(f"saving root index in {save_path}")
+        with open(os.path.join(save_path, "README.md"), "w", encoding="utf-8") as file:
+            file.write(root_index)
+
+
+def document_folder(
+    folder_path: str,
+    output_path: str,
+    model_name: str = DEFAULT_MODEL_NAME,
+) -> None:
+    save_path = os.path.join(output_path, folder_path)
     tales = []
 
-    logger.info(f"looking for dev files in folder {folder_path}")
     for filename in os.listdir(folder_path):
         file_path = os.path.join(folder_path, filename)
 
@@ -40,21 +73,23 @@ def document_folder(
             and os.path.splitext(filename)[1] in ALLOWED_EXTENSIONS
         ):
             logger.info(f"processing {file_path}")
-            file_tale = document_file(file_path)
+            file_tale = document_file(file_path, save_path)
 
             tales.append(
                 {"file_name": filename, "file_summary": file_tale["file_docstring"]}
             )
 
-    tales_index = get_tale_index(tales)
+    if tales:
+        tales_index = get_tale_index(tales)
+        if not os.path.exists(save_path):
+            os.makedirs(save_path)
 
-    save_path = os.path.join(output_path, folder_path)
-    if not os.path.exists(save_path):
-        os.makedirs(save_path)
+        logger.info(f"saving index in {save_path}")
+        with open(os.path.join(save_path, "README.md"), "w", encoding="utf-8") as file:
+            file.write(tales_index)
 
-    logger.info(f"saving index in {save_path}")
-    with open(os.path.join(save_path, "README.md"), "w", encoding="utf-8") as file:
-        file.write(tales_index)
+        return tales_index
+    return None
 
 
 def document_file(
@@ -91,9 +126,6 @@ def document_file(
     with open(save_path, "w") as json_file:
         json.dump(final_tale, json_file, indent=2)
 
-    # logger.info("AI-Generate Dev Tale:")
-    # pprint(final_tale)
-
     return final_tale
 
     # logger.info("add documentation to the code")
@@ -107,11 +139,11 @@ def document_file(
 
 @click.command()
 @click.option(
-    "-f",
-    "--folder-path",
-    "folder_path",
+    "-r",
+    "--repository-path",
+    "repository_path",
     required=True,
-    help="The path to the folder that contains the code files",
+    help="The path to the repository",
 )
 @click.option(
     "-o",
@@ -130,14 +162,13 @@ def document_file(
     help="The OpenAI model name you want to use. \
     https://platform.openai.com/docs/models",
 )
-def main(folder_path: str, output_path: str, model_name: str):
+def main(repository_path: str, output_path: str, model_name: str):
     if not os.environ.get("OPENAI_API_KEY"):
         os.environ["OPENAI_API_KEY"] = getpass.getpass(
             prompt="Enter your OpenAI API key: "
         )
 
-    # document_file(file_path, output_path, model_name)
-    document_folder(folder_path, output_path, model_name)
+    process_repository(repository_path, output_path, model_name)
 
 
 if __name__ == "__main__":
