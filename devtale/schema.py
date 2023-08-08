59c30643eeeaa28ff48b54e5b3cd723ef8a4cd81
Alberto Gaona <albertoo_3c@hotmail.com>
2023-08-08 12:28:12 -0600

    update descriptions in schema

--- devtale/schema.py
+++ devtale/schema.py
@@ -8,7 +8,7 @@ class ClassEntities(BaseModel):
     class_docstring: str = Field(
         default=None,
         description="Google Style Docstring text that provides an explanation of the \
-        purpose of the class and its class args. All inside the same str.",
+        purpose of the class, including its arguments if any. All inside the same str.",
     )
 
 
@@ -19,19 +19,21 @@ class MethodEntities(BaseModel):
     method_docstring: str = Field(
         default=None,
         description="Google Style Docstring text that provides an explanation of the \
-        purpose of the method/function, method args, method returns, and method \
-        raises. All inside the same str.",
+        purpose of the method/function, including its arguments, returns, and raises \
+        if any. All inside the same str.",
     )
 
 
 class FileDocumentation(BaseModel):
     classes: List[ClassEntities] = Field(
         default=None,
-        description="Entities containing class definitions along with their respective \
-        docstrings.",
+        description="List of entities containing class definitions along with their \
+        respective docstrings. This list does not include imports, utility classes, or \
+        class instances.",
     )
     methods: List[MethodEntities] = Field(
         default=None,
-        description="Entities containing method/function definitions along with their \
-        respective docstrings.",
+        description="List of entities containing method/function definitions along \
+        with their respective docstrings. This list does not include imports or \
+        method/function instances.",
     )
