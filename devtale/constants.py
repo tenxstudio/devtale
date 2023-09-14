2a04d43a9d27777bdef202c990a35d6c3aacb7b4
Alberto Gaona <albertoo_3c@hotmail.com>
2023-09-14 06:51:19 -0600

    add javascript as allowed extension

--- devtale/constants.py
+++ devtale/constants.py
@@ -1,24 +1,12 @@
 from langchain.text_splitter import Language
 
 # we are only documenting the file that ends with the following extensions:
-ALLOWED_EXTENSIONS = [".go", ".php", ".py", ""]
+ALLOWED_EXTENSIONS = [".js", ".go", ".php", ".py", ""]
 
 # split code files according the programming language
 LANGUAGES = {
     ".php": Language.PHP,
     ".py": Language.PYTHON,
     ".go": Language.GO,
-    ".cpp": Language.CPP,
-    ".java": Language.JAVA,
     ".js": Language.JS,
-}
-
-# indeitifers used to add documentation into the files.
-IDENTIFIERS = {
-    "php": [["class"], ["function"]],
-    "python": [["class"], ["def"]],
-    "go": [["struct"], ["func"]],
-    "cpp": [["class"], ["void", "int", "float", "double"]],
-    "java": [["class"], ["public", "protected", "private", "static"]],
-    "js": [["class"], ["function", "const", "let", "var"]],
 }
