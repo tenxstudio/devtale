b72a5361615142e7255f104eb556c5b6a7fa8dc9
Alberto Gaona <albertoo_3c@hotmail.com>
2023-09-14 12:48:25 -0600

    Add JavaScript Aggreator  (#49)
    * add javascript as allowed extension
    
    * add JS aggregator

--- devtale/aggregators/__init__.py
+++ devtale/aggregators/__init__.py
@@ -1,5 +1,6 @@
 from .go import GoAggregator
+from .javascript import JavascriptAggregator
 from .php import PHPAggregator
 from .python import PythonAggregator
 
-__all__ = ["PHPAggregator", "PythonAggregator", "GoAggregator"]
+__all__ = ["PHPAggregator", "PythonAggregator", "GoAggregator", "JavascriptAggregator"]
